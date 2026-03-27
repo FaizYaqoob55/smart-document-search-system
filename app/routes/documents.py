246bbc3fa44086a3621449127be0fe523ce51116
FaizYaqoob55 <faizyaqoob55@gmail.com>
2026-03-27 19:36:16 +0500

    Enhance document retrieval endpoints with error handling and dependency injection

--- app/routes/documents.py
+++ app/routes/documents.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, UploadFile,HTTPException,File
+from fastapi import APIRouter, Depends, UploadFile,HTTPException,File
 import os
 import shutil
-from app.database import SessionLocal
+
+from requests import Session
+from app.database import SessionLocal, get_db
 from app.services.text_extractor import (
     extract_text_from_pdf,
     chunks_text,
@@ -72,10 +74,33 @@ def upload_document(file: UploadFile = File(...)):
 
 
 
+@router.get("/")
+def get_documents(skip:int=0,limit:int=10,db:Session=Depends(get_db)):
+    documents=db.query(Document).offset(skip).limit(limit).all()
+    return documents
 
 
+@router.get("/{document_id}")
+def get_document(document_id:int,db:Session=Depends(get_db)):
+    document=db.query(Document).filter(Document.id==document_id).first()
+    if not document:
+        raise HTTPException(status_code=404, detail="Document not found")
+    return document
 
 
-
-
+@router.get("/{id}/chunks")
+def get_document_chunks(id:int,db:Session=Depends(get_db)):
+    chunks=db.query(DocumentChunk).filter(DocumentChunk.document_id==id).all()
+    if not chunks:
+        raise HTTPException(status_code=404, detail="Document chunks not found")
+    return [
+        {
+            "chunk_id": chunk.id,
+            "document_id": chunk.document_id,
+            "text": chunk.chunk_text,
+            "chunk_index": chunk.chunk_index,
+            # "embedding": chunk.embedding
+        } 
+        for chunk in chunks
+    ]
 
